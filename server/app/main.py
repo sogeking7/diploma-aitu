26596ce87c9580bc4e8df72a97be6d6bcf2540d7
Kaiyrkhan <kairolla222@gmail.com>
2025-05-15 04:14:02 +0500

    add cors

--- server/app/main.py
+++ server/app/main.py
@@ -36,6 +36,8 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:4200",
         "http://127.0.0.1:4200",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
