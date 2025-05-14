1cab11a974995a724f1f297852df4b4945ece39f
Kaiyrkhan <kairolla222@gmail.com>
2025-05-15 04:52:56 +0500

    Update CSS imports in App.tsx for improved styling consistency

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,17 +13,23 @@ import {
 import { IonReactRouter } from "@ionic/react-router";
 import { ellipse, square, triangle } from "ionicons/icons";
 
+/* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
+
+/* Basic CSS for apps built with Ionic */
 import "@ionic/react/css/normalize.css";
 import "@ionic/react/css/structure.css";
 import "@ionic/react/css/typography.css";
+
+/* Optional CSS utils that can be commented out */
 import "@ionic/react/css/padding.css";
 import "@ionic/react/css/float-elements.css";
 import "@ionic/react/css/text-alignment.css";
 import "@ionic/react/css/text-transformation.css";
 import "@ionic/react/css/flex-utils.css";
 import "@ionic/react/css/display.css";
-import "@ionic/react/css/palettes/dark.system.css";
+
+import "@ionic/react/css/palettes/dark.css";
 import "./theme/variables.css";
 
 import { AuthProvider, useAuth } from "./contexts/AuthContext/AuthContext";
